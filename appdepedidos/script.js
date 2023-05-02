6770597f6057919418de7df078ef90127dc6ab89
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-05-01 23:34:44 -0400

    creacion de archivo js en etiqueta script de html

---
+++ appdepedidos/script.js
@@ -0,0 +1,14 @@
+/*let platoElegido = prompt("¿Qué quieres comer hoy?");
+
+alert("Hoy es un ben día para comer " + platoElegido);
+
+let platoPrincipal = 1000;
+let bebida = 400;
+let totalPedido = platoPrincipal + bebida;*/
+
+let nombre = prompt("Hola, escribe tu nombre");
+alert("Hola "+ nombre + " bienvenido a la pagina de compra de cripto activos");
+
+let cantidad = prompt("cuando hardware wallets deseas comprar?");
+let precio = 4500;
+alert("El valor total de tu compra es "+ cantidad * precio);
