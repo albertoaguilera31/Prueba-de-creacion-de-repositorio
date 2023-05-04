dcb57b27e5aedfdb391a576124d891470c68de3b
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-05-03 22:01:11 -0400

    Se añade ejercicio sobre el ciclo for-in

--- appdepedidos/condicionales.js
+++ appdepedidos/condicionales.js
@@ -8,6 +8,17 @@ if(edad>=18){
 
 //codicional usando el operador ternario
 
-let edad = parseInt(prompt("¿cual es tu edad?"))
+/*let edad = parseInt(prompt("¿cual es tu edad?"))
 
-let resultado = (edad>=18)? console.log("puedes consumir alcohol"):console.log("solo puedes beber jugo o agua");
+let resultado = (edad>=18)? console.log("puedes consumir alcohol"):console.log("solo puedes beber jugo o agua");*/
+
+//Ejemplo del ciclo "for in"
+const user = {
+	name: "Alberto",
+  age: 36,
+  role: "JavaScript developer"
+}
+
+for (const prop in user) {
+	console.log(user[prop])
+}
