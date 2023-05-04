6cc6cb5f245cf440e9b13c1e4fd4e0ebdef46e78
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-05-03 20:36:25 -0400

    cambios en código usando codicional ternario

---
+++ appdepedidos/condicionales.js
@@ -0,0 +1,13 @@
+/*let edad=parseInt(prompt("¿Cual es tu edad?"));
+
+if(edad>=18){
+    console.log("puedes entrar a la disco");
+}else{
+    console.log("no puedes menor");
+}*/
+
+//codicional usando el operador ternario
+
+let edad = parseInt(prompt("¿cual es tu edad?"))
+
+let resultado = (edad>=18)? console.log("puedes consumir alcohol"):console.log("solo puedes beber jugo o agua");
--- condicionales.js
+++
@@ -1,7 +0,0 @@
-let edad=parseInt(prompt("¿Cual es tu edad?"));
-
-if(edad>=18){
-    console.log("puedes entrar a la disco");
-}else{
-    console.log("no puedes menor");
-}
